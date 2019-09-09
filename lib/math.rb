9cc41169aa96b9aa6390d5bf58330e384c1a0e1e
George <starcalluh@live.com>
2019-09-09 06:52:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,14 +1,20 @@
 def addition(num1, num2)
+  num1 + num2
 
 end
+addition
 
 def subtraction(num1, num2)
+  num1 - num2
 
 end
+subtraction
 
 def division(num1, num2)
+  num1 / num2 
 
 end
+division
 
 def multiplication(num1, num2)
 
